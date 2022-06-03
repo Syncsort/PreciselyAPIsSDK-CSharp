d88179d1643eb659b9f77b9b38238a61cd071cfd
Shubham Dubey <Shubham.Dubey@precisely.com>
2022-06-03 19:18:53 +0530

    PreciselyAPIs C# SDK Release 11.9.3

--- src/com.precisely.apis/Properties/AssemblyInfo.cs
+++ src/com.precisely.apis/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PreciselyAPIsSDKCSharp-11.9.2")]
+[assembly: AssemblyTitle("PreciselyAPIsSDKCSharp-11.9.3")]
 [assembly: AssemblyDescription("CSharp SDK for Precisely APIs")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Precisely")]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.9.2")]
-[assembly: AssemblyFileVersion("11.9.2")]
+[assembly: AssemblyVersion("11.9.3")]
+[assembly: AssemblyFileVersion("11.9.3")]
